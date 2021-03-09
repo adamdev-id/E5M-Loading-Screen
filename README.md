e058b3d4298eef4f7cdca46beff632c119072c4f
adamdev <id.adamdev@gmail.com>
2021-03-09 11:24:16 +0700

    main

---
+++ README.md
@@ -0,0 +1,3 @@
+# Website5
+EXTRIZZEN5M Loading Screen
+Demo: https://www.youtube.com/watch?v=kJ83kPNaeaY
---
+++ __resource.lua
@@ -0,0 +1,24 @@
+files {
+    'index.html',
+    'css/stylesheet.css',
+	'css/style.css',
+    'js/script.js',
+	'js/main.js',
+	'js/randombg.js',
+	'img/cursor.png',
+    'img/logo-main.png',
+	'img/discord.png',
+    'img/background1.jpg',
+	'img/background2.jpg',
+	'img/background3.jpg',
+	'img/background4.jpg',
+	'img/background5.jpg',
+	'img/background6.jpg',
+	'img/background7.jpg'
+}
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+client_script 'client.lua'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
